7f59be7b3f4a42cc170a75e8a3b2bcb6c39777c2
dh <kdhkdh1030@gmail.com>
2022-07-24 02:21:45 +0900

    팩토리 패턴 -기본

---
+++ src/디자인패턴/factory/pizzas/PizzaStore.java
@@ -0,0 +1,24 @@
+package 디자인패턴.factory.pizzas;
+
+public class PizzaStore {
+	SimplePizzaFactory factory;
+ 
+	public PizzaStore(SimplePizzaFactory factory) { 
+		this.factory = factory;
+	}
+ 
+	public Pizza orderPizza(String type) {
+		Pizza pizza;
+		
+		//new 연산자 대신 팩토리 객체에 있는 create메소드를 사용
+		pizza = factory.createPizza(type);
+ 
+		pizza.prepare();
+		pizza.bake();
+		pizza.cut();
+		pizza.box();
+
+		return pizza;
+	}
+
+}
